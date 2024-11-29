a460628964f4710ecbd9041a22d295bcd4c1b669
jion1990 <jionbofi@gmail.com>
2024-11-29 15:43:25 +0100

    Update index.js
    77777777777777777777

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public','index.html' )));
+app.use(express.static(path.join(__dirname, 'public' )));
 
 // إعداد بروكسي للطلبات
 app.use(
